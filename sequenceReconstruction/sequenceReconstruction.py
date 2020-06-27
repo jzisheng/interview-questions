022cb693a33522878dfc7ad65d46be3c200a827a
Jason <j.zisheng@gmail.com>
2020-06-27 15:04:21 -0400

    implementing dfs and keeping track num parents

--- sequenceReconstruction/sequenceReconstruction.py
+++ sequenceReconstruction/sequenceReconstruction.py
@@ -3,47 +3,31 @@ Check whether the original sequence org can be uniquely reconstructed from the s
 '''
 import collections
 class Solution:
-    def sequenceReconstruction(self, org, seqs):
-        self.buildGraph(seqs)
-        
-        # explore forwards, backwards dfs
-        a = self.dfsExplore(org)
-        b = self.dfsExplore(org[::-1])
-        print(a)
-        print(b)
-        return a == b
 
-    def dfsExplore(self,order):
-        self.visiting = set()
-        self.visited = set()
-        self.topo = []        
-        for node in order:
-            self.dfs(node)
-        return (self.topo[::-1])
-    
-    def dfs(self,node):
-        if node in self.visiting or node in self.visited:
-            # detects if cycle, or already visited
-            return
-        self.visiting.add(node)
-        for nei in self.graph[node]:
-            self.dfs(nei)
-            pass
-        self.visiting.remove(node)
-        self.visited.add(node)
-        self.topo.append(node)
-        pass
-        
-    def buildGraph(self,seqs):
-        self.graph = collections.defaultdict(list)        
+    def makeGraph(self,org,seqs):
+        self.graph = collections.defaultdict(list)
+        self.numParents = collections.defaultdict(int)
+        self.nodes = set()
         for seq in seqs:
-            if len(seq) == 1:
-                self.graph[seq[0]]
+            self.nodes = self.nodes.union(set(seq))
             for i in range(1,len(seq)):
                 a,b = seq[i-1],seq[i]
                 self.graph[a].append(b)
-                pass
+                self.numParents[b] += 1
+        pass
+    
+    def sequenceReconstruction(self, org, seqs):
+        self.makeGraph(org,seqs)
+        exploreNodes,res = [],[]        
+        # explore nodes with no parents
+        for node in org:
+            if self.numParents[node] == 0:
+                exploreNodes.append(node)
             pass
+        for node in exploreNodes:
+            self.dfs(node)
+        
+    def dfs(self,node):
         pass
     pass
 
@@ -52,29 +36,26 @@ class Solution:
 print("")
 s = Solution()
 
+# False
+org = [1,2,3]
+seqs = [[1,3],[1,2]]
 
+print(s.sequenceReconstruction(org,seqs))
+
+'''
 org = [4,1,5,2,6,3]
 seqs = [[5,2,6,3],[4,1,5,2]]
-print(s.sequenceReconstruction(org,seqs))
 
 
-# this should return false
-# two ways to construct:
-# [1,2,3]
-# [1,3,2]
-'''
-org, seqs = [1],[]
-print(s.sequenceReconstruction(org,seqs))
+org, seqs = [1], [[],[]]
 
+
+# False
 org = [1,2,3]
-seqs = [[1,2],[1,3]]
-print(s.sequenceReconstruction(org,seqs))
-
-
-
-org = [1,2,3]
-seqs = [[1,2]]
-print(s.sequenceReconstruction(org,seqs))
-
+seqs = [[2,3]]
 
 '''
+# false
+
+
+
