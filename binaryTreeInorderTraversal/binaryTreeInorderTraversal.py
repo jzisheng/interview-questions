a21ef52164249a998bee3da189c577d9ee6746d8
Jason <j.zisheng@gmail.com>
2020-06-27 21:38:20 -0400

    implemented iterative treenode stack

---
+++ binaryTreeInorderTraversal/binaryTreeInorderTraversal.py
@@ -0,0 +1,47 @@
+'''
+94. Binary Tree Inorder Traversal
+Medium
+
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,3,2]
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+'''
+from collections import *
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+    pass
+
+class Solution:
+    def inorderTraversal(self, root):
+        result, stack = [], []
+        # stack add left, root, val
+        curr = root
+        while curr != None or stack:
+            while curr != None:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            result.append(curr.val)
+            curr = curr.right
+        return result
+            
+
+a = TreeNode(10,left=TreeNode(5),right=TreeNode(15))
+s = Solution()
+print("")
+print(s.inorderTraversal(a))
