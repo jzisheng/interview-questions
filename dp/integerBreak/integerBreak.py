f59c19b8595a21379a4df935326347588bb9d8c0
Jason Chang <j.zisheng@gmail.com>
2020-07-01 09:03:35 -0400

    new dp problems

--- dp/integerBreak/integerBreak.py
+++ dp/integerBreak/integerBreak.py
@@ -16,11 +16,26 @@ Example 2:
 Input: 10
 Output: 36
 Explanation: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36.
-Note: You may assume that n is not less than 2 and not larger than 58.
+Note: You may assume that n is not less than 2 and not 
+larger than 58.
 
+
+dp[3]
+2+1
+1+1+1
+
+3-1
+2
 '''
 class Solution():
     def integerBreak(self,n):
-        pass
+        dp = [0]*(n+1)
+        for i in range(1,n+1):
+            for j in range(1,i):
+                dp[i] = max( dp[i],(i-j)*j,(i-j)*dp[j] )
+        return dp[n]
+    
     pass
 
+s = Solution()
+print(s.integerBreak(10))
