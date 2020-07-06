437dec36dfdbe7a901072b678d61e64c224e3061
Jason Chang <j.zisheng@gmail.com>
2020-07-06 10:32:09 -0400

    added better soln

--- dp/mincostTickets/mincostTickets.py
+++ dp/mincostTickets/mincostTickets.py
@@ -1,29 +1,33 @@
 class Solution:
-    def mincostTickets(self, days, costs):
-        dayset = set(days)
-        if not days:
-            return 0
+    
+    def mincostTickets(self,days,costs):
         memo = {}
-        def dp(day):
-            if day > 365:
+        if not days: return 0
+        def dp(idx,day):
+            if idx >= len(days):
                 return 0
-            if day in memo:
+            elif day in memo:
                 return memo[day]
-            elif day in dayset:
-                a = dp(day+1)+costs[0]
-                b = dp(day+7)+costs[1]
-                c = dp(day+30)+costs[2]
-                memo[day] = min(a,b,c)
-                return min(a,b,c)
+            elif days[idx] > 365:
+                return 0
+            elif days[idx] < day:
+                return dp(idx+1,day)
             else:
-                return dp(day+1)
-        return dp(days[0])
+                a = dp(idx+1,days[idx]+1)+costs[0]
+                b = dp(idx+1,days[idx]+7)+costs[1]
+                c = dp(idx+1,days[idx]+30)+costs[2]
+                result = min(a,b,c)
+                memo[day] = result
+                return result
+            pass
+        return dp(0,days[0])
     pass
 
 
+
 s = Solution()
-days = [1,4,6,7,8,20]
-costs = [2,7,15]
+days = [1,4,6,9,10,11,12,13,14,15,16,17,18,20,21,22,23,27,28]
+costs = [3,13,45]
 print(s.mincostTickets(days,costs))
 print(s.mincostTickets([],costs))
 
