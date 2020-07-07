a6a9d3e51575d213375339acf7754bf4d0acda6a
Jason Chang <j.zisheng@gmail.com>
2020-07-06 20:27:21 -0400

    job scheduling dp

--- dp/jobScheduling/jobScheduling.py
+++ dp/jobScheduling/jobScheduling.py
@@ -1,5 +1,23 @@
 
 class Solution:
     def jobScheduling(self,startTime,endTime,profit):
-        pass
-    pass
+        n = len(startTime)
+        jobs = sorted(zip(startTime, endTime, profit),\
+                      key=lambda v: v[1])
+        dp = [0] * (n+1)
+
+        for i, job in enumerate(jobs):
+            s1, e1, p1 = job[0], job[1], job[2]
+            dp[i+1] = p1
+            for j in range(i, -1, -1):
+                if jobs[j][1] <= s1:
+                    dp[i+1] = max(dp[i], dp[j+1] + job[2])
+                    break
+            dp[i+1] = max(dp[i], dp[i+1])
+        return dp[-1]
+
+s = Solution()
+startTime = [1,2,3,3]
+endTime = [3,4,5,6]
+profit = [50,10,40,70]
+print(s.jobScheduling(startTime,endTime, profit))
