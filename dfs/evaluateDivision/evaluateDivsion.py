407aa478e70a33b046621aef9a978e41d07f5113
Jason Chang <j.zisheng@gmail.com>
2020-07-10 15:58:21 -0400

    uploading all

--- dfs/evaluateDivision/evaluateDivsion.py
+++ dfs/evaluateDivision/evaluateDivsion.py
@@ -1,6 +1,10 @@
 '''
 399. Evaluate division
+
+
+DFS Solution:
 '''
+
 
 import collections
 
@@ -45,3 +49,58 @@ values = [2.0, 3.0]
 queries = [ ["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"] ]
 s = Solution()
 print(s.calcEquation(equations,values,queries))
+
+
+
+
+
+
+
+
+
+'''
+bfs solution
+'''
+import collections
+
+def makeGraph(equations, values):
+    graph = collections.defaultdict(list)
+    for idx, (a, b) in enumerate(equations):
+        weight = float(values[idx])
+        graph[a].append((b, weight))
+        graph[b].append((a, 1/weight))        
+    return graph
+
+
+def calcQuery(graph, query):
+    start, dest = query
+    visited = set()
+
+    if start not in graph: return -1.0
+    
+    def bfs(node, cost):
+        nonlocal dest, graph,visited
+        if node == dest:
+            return cost
+        visited.add(node)
+        for nei, weight in graph[node]:
+            if nei not in visited:
+                result = bfs(nei, cost*weight)
+                return result
+        return -1
+
+    
+    return bfs(start,1)
+    
+def calcEquation(equations, values, queries):
+    # Write your code here
+    graph = makeGraph(equations, values)
+    result = []
+    for query in queries:
+        result.append(float(calcQuery(graph,query)))
+    return result
+
+equations = [ ["a", "b"], ["b", "c"] ]
+values = [2.0, 3.0]
+queries = [ ["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"] ]
+print(calcEquation(equations,values,queries))
