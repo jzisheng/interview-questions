56d9aa714cf7f25eae2394dda9d8789106dfe605
Jason Chang <j.zisheng@gmail.com>
2020-07-13 11:03:12 -0400

    updated

--- bfs/findTheCity/findTheCity.py
+++ bfs/findTheCity/findTheCity.py
@@ -1,26 +1,48 @@
 import collections
+import heapq
 
 class Solution:
     def findTheCity(self, n, edges, distanceThreshold):
-        self.graph = collections.defaultdict(list)
-        for a, b, weight in edges:
-            self.graph[a].append((b, weight))
-        for a in self.graph:
-            self.graph[a].sort(key=lambda x:x[1])
-        
-        print(self.graph)
-        
-        for node in range(n):
-            self.bfs(node)
+        graph = collections.defaultdict(list)
+
+        for a, b, w in edges:
+            graph[a].append((b, w))
+            graph[b].append((a, w))
+
+        def numNeighbors(city):
+            queue = [(city, 0)]
+            visited = set()
+
+            while queue:
+                weight, node = heapq.heappop(queue)
+                if node in visited:
+                    continue
+                if node != city:
+                    visited.add(node)
+                for nei, nwei in graph[node]:
+                    if nei in visited:
+                        continue
+                    if weight+nwei <= distanceThreshold:
+                        heapq.heappush(queue, (weight+nwei, nei))
+            return len(visited)
+
+        res = []
+        for city in range(n):
+            res.append((numNeighbors(city), city))
+        res.sort(key=lambda x: (-x[0], x[1]))
+        return res[-1][1]
         pass
 
-    def bfs(self, node):
-        stack = []
-        pass
-
-
+    
 s = Solution()
 n = 4
 edges = [[0, 1, 3], [1, 2, 1], [1, 3, 4], [2, 3, 1]]
 distanceThreshold = 4
-s.findTheCity(n, edges, distanceThreshold)
+
+'''
+n = 5
+edges = [[0,1,2],[0,4,8],[1,2,3],[1,4,2],[2,3,1],[3,4,1]]
+distanceThreshold = 2
+'''
+
+print(s.findTheCity(n, edges, distanceThreshold))
