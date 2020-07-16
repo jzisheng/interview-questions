51763ed94253504c488bf6f50b8100b73630d841
Jason Chang <j.zisheng@gmail.com>
2020-07-16 16:16:32 -0400

    tabulated solution

--- dp/uniquePaths/uniquePaths.py
+++ dp/uniquePaths/uniquePaths.py
@@ -20,5 +20,13 @@ class Solution:
             return paths
         return f(0, 0)
 
+
+    def uniquePathsTabulated(self, m, n):
+        dp = [[1]*n for _ in range(m)]
+        for r in range(m-2,-1,-1):
+            for c in range(n-2,-1,-1):
+                dp[r][c] = dp[r+1][c] + dp[r][c+1]
+        return dp[0][0]
+
 s = Solution()
-s.uniquePaths(7, 3)
+print(s.uniquePathsTabulated(1, 1))
