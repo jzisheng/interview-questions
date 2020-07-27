072a26600ff9656b8dc573f9c8b4d4524b8cb086
Jason Chang <j.zisheng@gmail.com>
2020-07-27 09:31:02 -0400

    new arr problem threesum smaller

--- array/threeSumSmaller/threeSumSmaller.py
+++ array/threeSumSmaller/threeSumSmaller.py
@@ -1,7 +1,31 @@
+'''
+-2 0 1 3
+   ^  
+     ^ 
+       ^
+
+'''
+
 class Solution():
+    def twoSumSmaller(self, l, nums, target):
+        res = 0
+        r = len(nums)-1
+        while l < r:
+            if (nums[l]+nums[r]) < target:
+                res += r-l
+                l += 1
+            else:
+                r -= 1
+        return res
+    
     def threeSumSmaller(self, nums, target):
-        pass
-
-
-nums = [-2,0,1,3]
+        nums.sort()
+        result = 0
+        for i in range(0, len(nums)-2):
+            result += self.twoSumSmaller(i+1, nums, target-nums[i])
+        return result
+    
+s = Solution()
+nums = [-2, 0, 1, 3]
 target = 2
+s.threeSumSmaller(nums, target)
