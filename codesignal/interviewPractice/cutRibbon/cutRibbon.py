c7b6edd25760420d78324bf5b4d7546007faddf2
Jason Chang <j.zisheng@gmail.com>
2020-08-10 16:47:49 -0400

    refactor

--- codesignal/interviewPractice/cutRibbon/cutRibbon.py
+++ codesignal/interviewPractice/cutRibbon/cutRibbon.py
@@ -1,28 +1,20 @@
-
-def getLen(arr, target):
-    # get number of pieces that can be created
-    # with length k
+def getPieces(arr, ll):
+    # where ll is ribbon len
     res = 0
     for n in arr:
-        print("{} // {}  = {}".format(n,target, n//target))
-        res += n//target
+        res += n//ll
     return res
 
 def cutRibbon(arr, k):
-    mx = max(arr)
-    lo, hi = 1, mx
-    # Start with the largest size, if not possible
-    # then go to the next smallest
+    lo, hi = 0, max(arr)
     while lo <= hi:
-        mid = ( (hi-lo)/2 ) + lo
-        print("{} {} {}".format(lo,mid, hi))
-        ll = getLen(arr, mid)
-        if ll >= k:
+        mid = lo+((hi-lo)//2)
+        npieces = getPieces(arr,mid)
+        if npieces > mid:
             lo = mid+1
         else:
             hi = mid-1
     return hi
-        
 
 
 arr = [1, 2, 3, 4, 9]
